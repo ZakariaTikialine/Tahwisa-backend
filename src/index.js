b2e3dff80fdd1560b66d69a4f01cd98e30650ea8
zakaria <zakariatikia@gmail.com>
2025-07-04 15:41:13 +0100

    TahwisaBackend

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ app.use(cors(
     {
         origin: [
                     'http://localhost:3000',                
-                    'https://your-frontend.vercel.app'
+                    'https://tahwisa-frontend.vercel.app'
                 ],
         credentials: true
     }
