73d92a88cb566c577b7862973d646b3030b2779c
zakaria <zakariatikia@gmail.com>
2025-07-25 19:23:14 +0100

    backend

--- src/routes/resultatSelectionRoutes.js
+++ src/routes/resultatSelectionRoutes.js
@@ -18,12 +18,12 @@ const isAdmin = require('../middlewares/isAdminMiddleware');
 router.post('/', verifyToken, createResultatSelection);
 router.get('/', verifyToken, getAllResultatSelections);
 router.get('/eligible-employees', verifyToken, getEligibleEmployees);
-router.get('/:id', verifyToken, getResultatSelectionById);
-router.put('/:id', verifyToken, updateResultatSelection);
-router.delete('/:id', verifyToken, isAdmin, deleteResultatSelection);
 router.post('/selection/generate/:session_id', verifyToken, generateSelectionForSession);
 router.get('/session/:session_id', verifyToken, getResultatSelectionsBySession);
 router.get('/employee/:employee_id', verifyToken, getResultatSelectionsByEmployee);
 router.post('/winners/generate', verifyToken, isAdmin, generateWinnersForExpiredSessions);
+router.delete('/:id', verifyToken, isAdmin, deleteResultatSelection);
+router.get('/:id', verifyToken, getResultatSelectionById);
+router.put('/:id', verifyToken, updateResultatSelection);
 
 module.exports = router;
